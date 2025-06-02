c52d92dd0999e113be0730154b7cbfaac8e29e2e
donki <kincv@donki.com>
2025-06-02 14:14:41 +0900

    Remove pages/index.tsx to resolve routing conflict

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import KakaoChannel from '@/components/KakaoChannel';
-
-// ... existing code ...
-
-{/* Add this where you want the buttons to appear */}
-<KakaoChannel />
-
-// ... existing code ... 
